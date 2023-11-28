25419c4bfd6e5acf3ad46ffad0fec326301b0844
Melanie Winter <melaniebucher830@gmail.com>
2023-11-27 21:24:31 -0800

    Completed bonus work for deliverable

--- models/skill.js
+++ models/skill.js
@@ -1,18 +1,26 @@
 const skills = [
-    {id: 11, skill: 'HTML', level: 7},
-    {id: 12, skill: 'CSS', level: 7},
-    {id: 13, skill: 'JavaScript', level: 7},
-    {id: 14, skill: 'Node.js', level: 2},
-    {id: 15, skill: 'Express.js', level: 2},
-    {id: 16, skill: 'MongoDB', level: 0},
-    {id: 17, skill: 'React', level: 0},
+    {id: 11, skill: 'HTML', level: 7, done: true},
+    {id: 12, skill: 'CSS', level: 7, done: false},
+    {id: 13, skill: 'JavaScript', level: 7, done: false},
+    {id: 14, skill: 'Node.js', level: 2, done: false},
+    {id: 15, skill: 'Express.js', level: 2, done: false},
+    {id: 16, skill: 'MongoDB', level: 0, done: false},
+    {id: 17, skill: 'React', level: 0, done: false},
 ]
 
 module.exports = {
     getAll,
     getOne,
     create,
-    deleteOne
+    deleteOne,
+    update
+}
+
+function update (id, updatedSkill) {
+    id = parseInt(id)
+    const skill = skills.find(skill => skill.id === id)
+    Object.assign(skill, updatedSkill)
+
 }
 
 function deleteOne(id) {
@@ -24,6 +32,7 @@ function deleteOne(id) {
 function create(skill) {
     skill.id = Date.now() % 1000000
     skill.level = 0
+    skill.done = false
     skills.push(skill)
 }
 
